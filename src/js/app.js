c49984bae278cb0473f3ebe1b1c02ced1e517cc5
KhristSho <ShonusKhristo@yandex.ru>
2026-03-01 15:50:06 +0700

    fix2

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
-// import GameSavingLoader from "./gamesavingload";
+import GameSavingLoader from "./gamesavingload.js";
 
-// GameSavingLoader.load().then((saving) => {
-//   // saving объект класса GameSaving
-// }, (error) => {
-//   // ...
-// });
+GameSavingLoader.load().then((saving) => {
+    
+  console.log(saving);
+  
+}, (error) => {
+
+  throw new Error("Unlucky load");
+  
+});
